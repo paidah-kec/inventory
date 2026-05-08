20d0870b5de4cfc8f965cf851202dfcd07d303e1
paidah-kec <nurfaidahrahmah9f@gmail.com>
2026-05-09 04:40:56 +0800

    Add migrations, models, relations, resource controllers for items & categories

--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController; // <--- TAMBAHKAN BARIS INI
+use App\Http\Controllers\ItemController;     // <--- DAN INI JUGA
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('items', ItemController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/cek', function () {
-    return ["status" => "Aman!", "pesan" => "Web API Inventory sudah aktif"];
-});
