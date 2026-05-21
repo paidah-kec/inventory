997dd33c75744915afecb8214b49e6b4d4c6af6d
paidah-kec <nurfaidahrahmah9f@gmail.com>
2026-05-22 02:24:00 +0800

    Implement validation, error handling & service layer

--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/cek', function () {
-    return ["status" => "Aman!", "pesan" => "Web API Inventory sudah aktif"];
-});
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('items', ItemController::class);
