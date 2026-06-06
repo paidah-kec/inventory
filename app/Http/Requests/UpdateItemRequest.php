c39972c8f30c62e09cad3b539a976c22237b5583
paidah-kec <nurfaidahrahmah9f@gmail.com>
2026-06-06 08:44:11 +0800

    Add API versioning, BaseController, Input sanitization & documentation

--- app/Http/Requests/UpdateItemRequest.php
+++ app/Http/Requests/UpdateItemRequest.php
@@ -8,6 +8,18 @@ class UpdateItemRequest extends FormRequest {
     public function authorize() {
         return true;
     }
+    protected function prepareForValidation() {
+    $input = $this->all();
+
+    // Menyisir kiriman data dan melakukan trim serta strip_tags jika tipenya string
+        array_walk($input, function (&$val) {
+            if (is_string($val)) {
+                $val = trim(strip_tags($val));
+            }
+        });
+
+        $this->merge($input); // Memasukkan kembali data yang sudah bersih ke dalam request
+    }
     public function rules() {
         return [
             'name' => 'sometimes|required|string|max:255',
@@ -22,3 +34,4 @@ class UpdateItemRequest extends FormRequest {
         ];
     }
 }
+
